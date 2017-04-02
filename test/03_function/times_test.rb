0fd0ab8fcd95278686537827a244b868e44e9b15
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-02 21:42:33 +0900

    Add test for times function

--- test/03_function/times_test.rb
+++ test/03_function/times_test.rb
@@ -2,5 +2,16 @@ require 'test/unit'
 require_relative '../../src/03_function/times'
 
 class FunctionTest < Test::Unit::TestCase
-  
+  def test_yield
+    $stdout = StringIO.new
+    times(3){puts "test"}
+    actual = $stdout.string
+    expected = """test
+test
+test
+"""
+
+    assert_equal expected, actual
+    $stdout = STDOUT
+  end
 end
