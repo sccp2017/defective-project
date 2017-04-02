d145e50e2c172e2b7a0307b1682cddc50a6431b7
abab <ababup1192@gmail.com>
2017-04-03 00:23:12 +0900

    Apply Rubocop

--- test/02_expression/judge_number_position_test.rb
+++ test/02_expression/judge_number_position_test.rb
@@ -2,35 +2,35 @@ require 'test/unit'
 require_relative '../../src/02_expression/judge_number_position.rb'
 
 class ExpressionTest < Test::Unit::TestCase
-  def test_positive_1
+  def test_judge_number_position_1
     actual = judge_number_position(1)
     expected = 'positive'
 
     assert_equal expected, actual
   end
 
-  def test_positive_2
-    actual = judge_number_position(1234567)
+  def test_judge_number_position_positive_2
+    actual = judge_number_position(1_234_567)
     expected = 'positive'
 
     assert_equal expected, actual
   end
 
-  def test_negative_1
+  def test_judge_number_position_negative_1
     actual = judge_number_position(-1)
     expected = 'negative'
 
     assert_equal expected, actual
   end
 
-  def test_negative_2
-    actual = judge_number_position(-1234567)
+  def test_judge_number_position_negative_2
+    actual = judge_number_position(-1_234_567)
     expected = 'negative'
 
     assert_equal expected, actual
   end
 
-  def test_zero
+  def test_judge_number_position_zero
     actual = judge_number_position(0)
     expected = 'zero'
 
--- test/03_function/times_test.rb
+++ test/03_function/times_test.rb
@@ -2,9 +2,9 @@ require 'test/unit'
 require_relative '../../src/03_function/times'
 
 class FunctionTest < Test::Unit::TestCase
-  def test_yield
+  def test_times
     $stdout = StringIO.new
-    times(3){puts "test"}
+    times(3) { puts 'test' }
     actual = $stdout.string
     expected = <<EOS
 test
