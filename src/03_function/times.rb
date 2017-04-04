e196a3987f61d8a6643529de81fe7184b232b1c2
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-04 19:24:01 +0900

    Modify times

--- src/03_function/times.rb
+++ src/03_function/times.rb
@@ -1,9 +1,3 @@
-# Call given block n times
 def times(n)
-  # TODO: It's so exasperating!
-  # Can you replace FOR LOOP with another expression?
-  # Of course you shouldn't use times method in Ruby.
-  for i in 0..n
-    yield
-  end
+  # Call given block n times
 end
