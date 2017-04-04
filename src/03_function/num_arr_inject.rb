8b5374a04ca8ee491f8ef6ed9107cb03f9912d71
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-05 01:11:20 +0900

    Apply rubocop to num_arr_inject on 03

--- src/03_function/num_arr_inject.rb
+++ src/03_function/num_arr_inject.rb
@@ -1,9 +1,7 @@
-# Combines and return all elements of given array by applying a binary operation which specified by a block
-# Call like:
-# num_arr_inject([1,2,3], 0){|acc, n| acc + n}
-# => 15
+# Combines all elements of given array recursively
+# by applying a binary operation which specified by a block.
 def num_arr_inject(arr, m, &block)
-  if arr.empty? then
+  if arr.empty?
     m
   else
     # What value or variable should be placed in arguments of yield statement?
