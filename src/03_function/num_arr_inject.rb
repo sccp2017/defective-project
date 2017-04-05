4751fa44fba7d1f8c862db6712d9b10cf20af72f
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-05 17:30:19 +0900

    Modify num_arr_inject

--- src/03_function/num_arr_inject.rb
+++ src/03_function/num_arr_inject.rb
@@ -2,9 +2,6 @@
 # by applying a binary operation which specified by a block.
 def num_arr_inject(arr, m, &block)
   if arr.empty?
-    m
   else
-    # What value or variable should be placed in arguments of yield statement?
-    num_arr_inject(arr[1..-1], yield(), &block)
   end
 end
