9f2e9abdb74aaa6ae4832e0f26dc3a9dcbb892de
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-07 21:26:40 +0900

    Fix mis function call on sec_to_time_test func

--- test/01_type/sec_to_time_test.rb
+++ test/01_type/sec_to_time_test.rb
@@ -3,7 +3,7 @@ require_relative '../../src/01_type/sec_to_time'
 
 class TypeTest < Test::Unit::TestCase
   def test_sec_to_time_cond1
-    actual = round_ones_place(46_979)
+    actual = sec_to_time(46_979)
     expected = '13:2:59'
 
     assert_equal expected, actual
