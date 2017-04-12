c2585f464478616d511ca9339ac2307d457d8e53
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-12 18:38:11 +0900

    Add test case to is_number func on 02

--- test/02_expression/is_number_test.rb
+++ test/02_expression/is_number_test.rb
@@ -2,9 +2,16 @@ require 'test/unit'
 require_relative '../../src/02_expression/is_number.rb'
 
 class ExpressionTest < Test::Unit::TestCase
-  def test_is_number
+  def test_is_number_cond1
     actual = is_number?(15)
     expected = true
+
+    assert_equal expected, actual
+  end
+
+  def test_is_number_cond2
+    actual = is_number?('test')
+    expected = false
 
     assert_equal expected, actual
   end
