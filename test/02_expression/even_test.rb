98eae812031f9858a6482ab0432da6a31802bbab
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-04-14 19:20:08 +0900

    Add test for even func on 02

---
+++ test/02_expression/even_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require_relative '../../src/02_expression/even.rb'
+
+class ExpressionTest < Test::Unit::TestCase
+  def test_even_cond1
+    actual = even?(889)
+    expected = false
+
+    assert_equal expected, actual
+  end
+
+  def test_even_cond2
+    actual = even?(464)
+    expected = true
+
+    assert_equal expected, actual
+  end
+end
