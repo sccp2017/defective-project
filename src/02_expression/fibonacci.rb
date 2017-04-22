0fa35c33ffff429dca02f69a32bea2c51e2f9b59
moti <motohiko.ave@gmail.com>
2017-04-22 17:44:11 +0900

    Add 02 fibonacci problem

---
+++ src/02_expression/fibonacci.rb
@@ -0,0 +1,7 @@
+def fibonacci(num)
+  return 0 if num.zero?
+  return 1 if num == 1
+  a = 0
+  b = 1
+  c = 1
+end
---
+++ test/02_expression/fibonacci_test.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require_relative '../../src/02_expression/fibonacci.rb'
+
+class ExpressionTest < Test::Unit::TestCase
+  def test_fibonacci_1
+    actual = fibonacci(3)
+    expected = 2
+
+    assert_equal expected, actual
+  end
+
+  def test_fibonacci_2
+    actual = fibonacci(20)
+    expected = 6765
+
+    assert_equal expected, actual
+  end
+
+  def test_fibonacci_3
+    actual = fibonacci(0)
+    expected = 0
+
+    assert_equal expected, actual
+  end
+
+  def test_fibonacci_4
+    actual = fibonacci(1)
+    expected = 1
+
+    assert_equal expected, actual
+  end
+end
