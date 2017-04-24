d5ae5ba8e191dbb095b1d153340cdfccb0440995
haji <s1230149@gmail.com>
2017-04-25 00:52:49 +0900

    #60 fix number_of_substring

--- test/02_expression/number_of_substring_test.rb
+++ test/02_expression/number_of_substring_test.rb
@@ -9,35 +9,35 @@ class ExpressionTest < Test::Unit::TestCase
   end
 
   def test_number_of_substring2
-    actual = number_of_substring("abcdefg","abe")
+    actual = number_of_substring("abcdefg", "abe")
     expected = 0
 
     assert_equal expected, actual
   end
 
   def test_number_of_substring3
-    actual = number_of_substring("abracadabra","ab")
+    actual = number_of_substring("abracadabra", "ab")
     expected = 2
     
     assert_equal expected, actual
   end
 
   def test_number_of_substring4
-    actual = number_of_substring("ababab","aba")
+    actual = number_of_substring("ababab", "aba")
     expected = 2
 
     assert_equal expected, actual
   end
 
   def test_number_of_substring5
-    actual = number_of_substring("abcde","abcdefg")
+    actual = number_of_substring("abcde", "abcdefg")
     expected = 0
 
     assert_equal expected, actual
   end
 
   def test_number_of_substring6
-    actual = number_of_substring("ab ab  ab abc ","ab ")
+    actual = number_of_substring("ab ab  ab abc ", "ab ")
     expected = 3
 
     assert_equal expected, actual
