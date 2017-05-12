e16bd0f91846620e9ff88427a6158020088f1d03
uzimaru <uzimaru0601@gmail.com>
2017-05-12 12:39:54 +0900

    Fix sec_to_time

--- src/01_type/sec_to_time.rb
+++ src/01_type/sec_to_time.rb
@@ -1,4 +1,8 @@
 # sec -> hh:mm:ss
 def sec_to_time(sec)
-  "#{}:#{}:#{}"
+    hour = sec / 3600
+    sec -= hour * 3600
+    min = sec / 60
+    sec -= min * 60
+    "#{"%02d" %hour}:#{"%02d" %min}:#{"%02d" %sec}"
 end
