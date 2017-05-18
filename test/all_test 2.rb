1014c13788ad43720d5fbcb1dcc1fe439b8adcef
abab <ababup1192@gmail.com>
2017-05-18 12:58:23 +0900

    Delete files

--- test/all_test 2.rb
+++
@@ -1,6 +0,0 @@
-require 'test/unit'
-require_relative '01_type/all_test'
-require_relative '02_expression/all_test'
-require_relative '03_function/all_test'
-require_relative '04_array/all_test'
-require_relative '05_exception/all_test'
