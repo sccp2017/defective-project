64a6078e179cd4237d0bd387cd09767aecd2a19a
michael <micfall.romtin@gmail.com>
2017-05-18 12:51:00 +0900

    add circle_area problem

---
+++ test/02_expression/circle_area_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require_relative '../../src/02_expression/circle_area'
+
+class FunctionTest < Test::Unit::TestCase
+  def test_circle_area1
+    actual = circle_area(10)
+    expected = 10 * 10 * Math::PI
+
+    assert (expected - actual) < 0.1
+  end
+
+  def test_circle_area2
+    actual = circle_area(1)
+    expected = Math::PI
+
+    assert_equal actual, expected
+  end
+end
