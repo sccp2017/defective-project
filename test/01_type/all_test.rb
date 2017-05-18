902b0756fa9cc603e01875cc9b69ac785ab8217e
michael <micfall.romtin@gmail.com>
2017-05-18 15:42:19 +0900

    tmp

--- test/01_type/all_test.rb
+++ test/01_type/all_test.rb
@@ -1,3 +1,3 @@
 require 'test/unit'
 Dir['./*.rb'].reject { |file| file == 'all_test.rb' }
-.each { |file| require_relative file }
+.each { |file| require_relative File.expand_path(file) }
--- test/all_test.rb
+++ test/all_test.rb
@@ -1,6 +1,2 @@
 require 'test/unit'
-require_relative '01_type/all_test'
-require_relative '02_expression/all_test'
-require_relative '03_function/all_test'
-require_relative '04_array/all_test'
-require_relative '05_exception/all_test'
+Dir['*/all_test.rb'].each { |f| puts File.expand_path(f); require_relative File.expand_path(f) }
