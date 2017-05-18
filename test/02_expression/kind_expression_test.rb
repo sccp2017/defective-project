533551d7fdc97ef24db9be30748433aede22088d
abab <ababup1192@gmail.com>
2017-05-18 14:30:32 +0900

    Modify test code

--- test/02_expression/kind_expression_test.rb
+++ test/02_expression/kind_expression_test.rb
@@ -9,51 +9,51 @@ class ExpressionTest < Test::Unit::TestCase
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond2
+  def test_kind_expression_cond2
     actual = kind_expression('x-y')
-    expected = 'diff'
+    expected = 'sub'
 
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond3
+  def test_kind_expression_cond3
     actual = kind_expression('x*y')
     expected = 'mul'
 
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond4
+  def test_kind_expression_cond4
     actual = kind_expression('x/y')
     expected = 'div'
 
     assert_equal expected, actual
   end
-  
- def test_kind_expression_cond5
+
+  def test_kind_expression_cond5
     actual = kind_expression('(x+y)')
     expected = 'bracket add'
 
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond6
+  def test_kind_expression_cond6
     actual = kind_expression('(x-y)')
-    expected = 'bracket diff'
+    expected = 'bracket subg'
 
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond7
+  def test_kind_expression_cond7
     actual = kind_expression('(x*y)')
     expected = 'bracket mul'
 
     assert_equal expected, actual
   end
 
- def test_kind_expression_cond8
+  def test_kind_expression_cond8
     actual = kind_expression('(x/y)')
-    expected = 'bracket diff'
+    expected = 'bracket div'
 
     assert_equal expected, actual
   end
