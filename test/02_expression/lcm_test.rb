75a93ef8721b50c7010e6e8dff57c37e5caf5414
misoton665 <misoton998@gmail.com>
2017-05-25 19:06:56 +0900

    Fix divisor_num -> lcm

--- test/02_expression/lcm_test.rb
+++ test/02_expression/lcm_test.rb
@@ -2,29 +2,29 @@ require 'test/unit'
 require_relative '../../src/02_expression/lcm'
 
 class ExpressionTest < Test::Unit::TestCase
-  def test_gcd_cond1
-    actual = divisor_num(5, 7)
+  def test_lcm_cond1
+    actual = lcm(5, 7)
     expected = 35
 
     assert_equal expected, actual
   end
 
-  def test_gcd_cond2
-    actual = divisor_num(14, 35)
+  def test_lcm_cond2
+    actual = lcm(14, 35)
     expected = 70
 
     assert_equal expected, actual
   end
 
-  def test_gcd_cond3
-    actual = divisor_num(12, 18)
+  def test_lcm_cond3
+    actual = lcm(12, 18)
     expected = 36
 
     assert_equal expected, actual
   end
 
-  def test_gcd_cond4
-    actual = divisor_num(18, 27)
+  def test_lcm_cond4
+    actual = lcm(18, 27)
     expected = 54
 
     assert_equal expected, actual
