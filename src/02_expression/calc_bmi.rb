2f94f0a83778f8ec4591aeebbcb1ce81f6bd2e27
s1250207-yoshida <s1250207@u-aizu.ac.jp>
2017-06-01 19:04:11 +0900

    fix 02_expression/calc_bmi.rb

--- src/02_expression/calc_bmi.rb
+++ src/02_expression/calc_bmi.rb
@@ -3,6 +3,17 @@
 # Overweight = 25~29.9
 # Obesity = BMI of 30 or greater
 def calc_bmi(height, weight)
-  bmi = weight / (height**2)
+  bmi = weight.to_f / (height.to_f/10**2)**2
   # Show me your process which calculate BMI here.
+  a = if bmi < 18.5 then
+        "Underweight"
+      elsif bmi >= 18.5 && bmi < 25 then
+        "Normal weight"
+      elsif bmi >= 25 && bmi < 30 then
+        "Overweight"
+      else
+        "Obesity"
+      end
+  "#{a}"
+  
 end
