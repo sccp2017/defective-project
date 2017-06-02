aecaa3ff3a071991e2d3465826d37e62ed679783
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-02 17:15:33 +0900

    Fix 02_expression/grade.rb

--- src/02_expression/grade.rb
+++ src/02_expression/grade.rb
@@ -1,3 +1,16 @@
 # point(0 to 100) to grade('A' to 'F' or nil)
 def grade(point)
+    if !point.between?(0, 100)
+        nil
+    elsif point.between?(0, 19)
+        "F" 
+    elsif point.between?(20, 39)
+        "D"
+    elsif point.between?(40, 59)
+        "C"
+    elsif point.between?(60, 79)
+        "B"
+    elsif point.between?(80, 100)
+        "A"
+    end
 end
