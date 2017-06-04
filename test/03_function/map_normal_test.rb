ddd711c63bfed741c3c2332d07ba6f130ff6c9c3
abab <ababup1192@gmail.com>
2017-06-04 16:11:18 +0900

    Add map_normal

---
+++ test/03_function/map_normal_test.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require_relative '../../src/03_function/map_normal'
+
+class FunctionTest < Test::Unit::TestCase
+  def test_map_cond1
+    actual = map([]) { |x| x * 2 }
+    expected = []
+
+    assert_equal expected, actual
+  end
+
+  def test_map_cond2
+    actual = map((1..5).to_a) { |x| x * 2 }
+    expected = [2, 4, 6, 8, 10]
+
+    assert_equal expected, actual
+  end
+
+  def test_map_cond3
+    actual = map((1..5).to_a, &:to_s)
+    expected = %w(1 2 3 4 5)
+
+    assert_equal expected, actual
+  end
+
+  def test_map_cond4
+    actual = map(%w(1 2 3 4 5), &:to_i)
+    expected = [1, 2, 3, 4, 5]
+
+    assert_equal expected, actual
+  end
+end
