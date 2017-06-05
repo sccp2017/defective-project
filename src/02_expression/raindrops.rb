41f0723624ab9ff4a906c70817d42ee4c1936337
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-05 16:21:36 +0900

    Fix 02_expression/raindrops.rb

--- src/02_expression/raindrops.rb
+++ src/02_expression/raindrops.rb
@@ -1,3 +1,8 @@
 def raindrops(number)
-  true
+    str = ""
+    str += 'Pling' if number % 3 == 0
+    str += 'Plang' if number % 5 == 0
+    str += 'Plong' if number % 7 == 0
+    str += number.to_s if str.empty?
+    str
 end
