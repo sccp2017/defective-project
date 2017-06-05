f4bad3f0c4501ea3d233c5a62435f0f010652579
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-05 15:30:23 +0900

    Fix 02_expression/grade.rb

--- src/02_expression/grade.rb
+++ src/02_expression/grade.rb
@@ -1,16 +1,30 @@
 # point(0 to 100) to grade('A' to 'F' or nil)
 def grade(point)
-    if !point.between?(0, 100)
-        nil
-    elsif point.between?(0, 19)
-        "F" 
-    elsif point.between?(20, 39)
+    #if !point.between?(0, 100)
+    #    nil
+    #elsif point.between?(0, 19)
+    #    "F" 
+    #elsif point.between?(20, 39)
+    #    "D"
+    #elsif point.between?(40, 59)
+    #    "C"
+    #elsif point.between?(60, 79)
+    #    "B"
+    #elsif point.between?(80, 100)
+    #    "A"
+    #end
+    case point
+    when 0..19
+        "F"
+    when 20..39
         "D"
-    elsif point.between?(40, 59)
+    when 40..59
         "C"
-    elsif point.between?(60, 79)
+    when 60..79
         "B"
-    elsif point.between?(80, 100)
+    when 80..100
         "A"
+    else
+        nil
     end
 end
