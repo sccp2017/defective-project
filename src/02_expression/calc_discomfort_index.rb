9039111f2df1c065c5adacafb16788ceb445f801
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-07 12:12:46 +0900

    fix-02/calc_discomfort_index

--- src/02_expression/calc_discomfort_index.rb
+++ src/02_expression/calc_discomfort_index.rb
@@ -2,5 +2,20 @@
 # H: humidity
 # ref: http://keisan.casio.com/exec/system/1351058230
 def calc_discomfort_index(temp, humid)
+  
   discomfort_index = temp - 0.55 * (1 - 0.01 * humid) * (temp - 14.5)
+  case discomfort_index
+  when 0..21 
+    "No discomfort."
+  when 21..24
+    'Under 50% of population feels discomfort.'
+  when 24..27
+    'Most 50% of population feels discomfort.'
+  when 27..29
+    'Most of population suffers discomfort.'
+  when 29..32
+    'Everyone feels severe stress'
+  else
+    'State of medical emergency'
+  end
 end
