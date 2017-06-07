a0484479f75c4c6f89f7ba56b6354942325f200a
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-08 03:45:31 +0900

    Fix 03_function/gcd_lcm.rb

--- src/03_function/gcd_lcm.rb
+++ src/03_function/gcd_lcm.rb
@@ -1,15 +1,28 @@
 def gcdOfTwo(n, m)
     #Implement with recursion.
+    return m if n % m == 0
+    gcdOfTwo(n, n % m)
 end
 
 def gcd(*nums)
     #Implement with a certain higher order function and gcdOfTwo.
+    g = gcdOfTwo(nums.max, nums.min)
+    return g if nums.length == 2
+    nums.delete(nums.max)
+    nums.delete(nums.min)
+    gcd(g, *nums)
 end
 
 def lcmOfTwo(n, m)
     #Implement with gcdOfTwo.
+    n * m / gcdOfTwo(n, m)
 end
 
 def lcm(*nums)
     #Implement with a certain higher order function and lcmOfTwo.
+    l = lcmOfTwo(nums.max, nums.min)
+    return l if nums.length == 2
+    nums.delete(nums.max)
+    nums.delete(nums.min)
+    lcm(l, *nums)
 end
