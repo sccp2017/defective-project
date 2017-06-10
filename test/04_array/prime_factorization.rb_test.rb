6fd00e2caf54fc65e6d3ea604188bdadc3a75449
skht777 <skht@skhtlab.com>
2017-06-11 08:31:10 +0900

    fix test cases

--- test/04_array/prime_factorization.rb_test.rb
+++ test/04_array/prime_factorization.rb_test.rb
@@ -10,21 +10,28 @@ class FunctionTest < Test::Unit::TestCase
   end
 
   def test_factorize2()
-    expected = [7, 13, 19, 29, 37]
+    expected = [7, 13, 19, 29, 37, 43, 53, 61, 97]
     actual = primeFactorization(expected.inject(:*))
 
     assert_equal expected, actual
   end
 
   def test_factorize3()
-    expected = [97, 109, 131, 149]
+    expected = [109, 283, 353, 467, 599, 661, 773, 811, 997]
     actual = primeFactorization(expected.inject(:*))
 
     assert_equal expected, actual
   end
 
   def test_factorize4()
-    expected = [2, 4639, 9973]
+    expected = [2, 7, 2017, 3187, 4639, 5471, 6029, 7331, 8663, 9973]
+    actual = primeFactorization(expected.inject(:*))
+
+    assert_equal expected, actual
+  end
+
+  def test_factorize5()
+    expected = [2017, 8689, 24077, 53527, 64601, 83933, 99881]
     actual = primeFactorization(expected.inject(:*))
 
     assert_equal expected, actual
