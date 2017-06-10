658ee8b240f1d5760c846ecf4f1018e2c86d0211
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-10 13:04:32 +0900

    Fix 03_function/map_normal.rb

--- src/03_function/map_normal.rb
+++ src/03_function/map_normal.rb
@@ -1,7 +1,10 @@
 # Combines all elements of given array recursively
 # by applying a binary operation which specified by a block.
 def map(arr, &block)
-    return arr if arr.empty?
-    n = block.call arr.pop
-    return map(arr, &block) << n
+    if arr.empty?
+        arr
+    else
+        n = block.call arr.pop
+        map(arr, &block) << n
+    end
 end
