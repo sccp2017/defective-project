2d94fd0f9bc06eb0f04138dec05426283b6fb8fb
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-12 11:48:11 +0900

    fix 03/forall_hard

--- src/03_function/forall_hard.rb
+++ src/03_function/forall_hard.rb
@@ -1,4 +1,14 @@
 # Your can not use loop and recursive.
 
-def forall(arr)
+def forall(arr, &block)
+  if arr.empty?
+    true
+  else
+    head, *tail = arr
+    if yield(head)
+      forall(tail, &block)
+    else
+      false
+    end
+  end
 end
