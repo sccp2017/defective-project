ae8c7c4ecc8e556951511a248ba1269de5ccb6a6
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-12 11:07:24 +0900

    fix 03/concat_3strings_easy

--- src/03_function/concat_3strings_easy.rb
+++ src/03_function/concat_3strings_easy.rb
@@ -1,15 +1,7 @@
 def concat_3strings(str1, str2, str3)
-<<<<<<< HEAD
   concat(str1, str2) + str3
 end
 
 def concat(str1, str2)
   str1 + str2
-=======
- str1+str2+str3
-end
-
-def concat(str1, str2)
- str1+str2
->>>>>>> d9ddb6bc5beb18f5fc03650f492f75e99f2fcd20
 end
