f4ea22a65f7ebf14839e3d607f671ba00abf608d
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-12 23:07:23 +0900

    fix 03/exists_hard

--- src/03_function/exists_hard.rb
+++ src/03_function/exists_hard.rb
@@ -1,4 +1,14 @@
 # Your can not use loop and recursive.
 
-def exists(arr)
+def exists(arr, &block)
+  if arr.empty?
+    false
+  else
+    head, *tail = arr
+    if yield(head)
+      true
+    else
+      exists(tail, &block)
+    end
+  end
 end
