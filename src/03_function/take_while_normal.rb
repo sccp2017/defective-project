b3b93401e5bbc368c9c01f947ad87f43bb9e469f
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-12 22:37:05 +0900

    Fix 03_function/take_while_normal.rb

--- src/03_function/take_while_normal.rb
+++ src/03_function/take_while_normal.rb
@@ -1,7 +1,10 @@
 # Combines all elements of given array recursively
 # by applying a binary operation which specified by a block.
 def take_while(arr, &block)
-  if arr.empty?
-  else
-  end
+    head, *tail = arr
+    if !arr.empty? && yield(head)
+        [head] + take_while(tail, &block)
+    else
+        []
+    end
 end
