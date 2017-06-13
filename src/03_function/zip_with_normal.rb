a3a6285fbbd8f7e06fd7e123070bad4655b70e21
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-13 20:36:56 +0900

    fix 03/zip_with_normal

--- src/03_function/zip_with_normal.rb
+++ src/03_function/zip_with_normal.rb
@@ -1,1 +1,9 @@
-# Write down your code here.
+def zip_with(arr1, arr2, &block)
+  if arr1.empty? || arr2.empty?
+    []
+  else
+    head1, *tail1 = arr1
+    head2, *tail2 = arr2
+    [yield(head1, head2)] + zip_with(tail1, tail2, &block)
+  end
+end
