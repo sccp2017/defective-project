e8df006f074119b8255422bf1c8f8203db4a61d6
manga_osyo <manga.osyo@gmail.com>
2017-06-14 23:58:35 +0900

    Add 04_array/range.

---
+++ test/04_array/range.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require_relative '../../src/04_array/range'
+
+class FunctionTest < Test::Unit::TestCase
+  def test_range1
+    actual = range(1, 5)
+    expected = [1, 2, 3, 4, 5]
+
+    assert_equal expected, actual
+  end
+
+  def test_range2
+    actual = range(-2, 2)
+    expected = [-2, -1, 0, 1, 2]
+
+    assert_equal expected, actual
+  end
+
+  def test_range3
+    actual = range(2, -2)
+    expected = []
+
+    assert_equal expected, actual
+  end
+end
