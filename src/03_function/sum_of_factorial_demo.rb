67c75dce9b10727718123dd63e376f80dbee58d7
sXs1250131-Kimura <s1250131@u-aizu.ac.jp>
2017-06-15 19:33:58 +0900

    fix 03_function/sum_of_factorial

--- src/03_function/sum_of_factorial_demo.rb
+++ src/03_function/sum_of_factorial_demo.rb
@@ -1,1 +1,11 @@
-# You must not solve this problem until 6/15 6 period.
+def sum_of_factorial(arr)
+  sum = 0
+  for x in arr
+    pro = 1
+    for y in 1..x
+      pro *= y
+    end
+    sum += pro
+  end
+    sum
+end
