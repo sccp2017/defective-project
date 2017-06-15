acd0b079bf76f019d04cc4852f516173f208a5e4
sXX1250141-Hida <s1250141@u-aizu.ac.jp>
2017-06-15 19:15:03 +0900

    solved fibonacci

--- src/02_expression/fibonacci.rb
+++ src/02_expression/fibonacci.rb
@@ -4,4 +4,15 @@ def fibonacci(num)
   a = 0
   b = 1
   c = 1
+
+  sum = []
+  sum << 0
+  sum << 1
+
+  if num>1 then 
+    for i in 2..num do
+      sum << sum[i-2] + sum[i-1]
+    end
+   return sum[num]
+  end
 end
