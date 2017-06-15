3f8bf2e42915c9226d5912a828b6eab1f8a6d1fb
michael <micfall.romtin@gmail.com>
2017-06-15 18:24:01 +0900

    solved

--- src/03_function/sum_of_factorial_demo.rb
+++ src/03_function/sum_of_factorial_demo.rb
@@ -1,1 +1,12 @@
 # You must not solve this problem until 6/15 6 period.
+def factorial(n)
+  if n == 1
+    1
+  else
+    n * factorial(n-1)
+  end
+end
+
+def sum_of_factorial(arr)
+  arr.inject(0){|sum, n| sum + factorial(n)}
+end
