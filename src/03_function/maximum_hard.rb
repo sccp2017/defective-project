c50e9c3f899de02099cc13f4f8ed0a17bc89be18
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-19 09:31:10 +0900

    Fix 03_function/maximum_hard.rb

--- src/03_function/maximum_hard.rb
+++ src/03_function/maximum_hard.rb
@@ -2,5 +2,5 @@ require_relative './inject_normal'
 
 # You can not use loop and recursive.
 def maximum(arr)
-    inject(arr, 0){ |acc, x| acc < x ? x : acc }
+    inject(arr, Float::MIN){ |acc, x| acc < x ? x : acc }
 end
