d56fa75f8cec0a4fd956f042a4be42099694a3f0
s1250061-Matsumoto <s1250061@u-aizu.ac.jp>
2017-06-22 19:20:47 +0900

    fix 03_function/nest_sum_demo

--- src/03_function/nest_sum_demo.rb
+++ src/03_function/nest_sum_demo.rb
@@ -1,5 +1,16 @@
 # hint: use .class
 
 def nest_sum(arr)
+  res = 0
+  for i in arr
+    if i.class == Fixnum then
+      res += i
+    else
+      for j in i
+        res += j
+      end
+    end
+  end
+  res
 end
 
