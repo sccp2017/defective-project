08d39c60f9d802fe681fc163d9df61ac187db52f
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-25 01:18:27 +0900

    fix 02/frame.rb

--- src/02_expression/frame.rb
+++ src/02_expression/frame.rb
@@ -3,13 +3,13 @@ def frame(h, w)
 
     for y in 0..h-1 do
         for x in 0..w-1 do
-            str += if x % (h-1) == 0 || y % (w-1) == 0 then
+            str += if y % (h-1) == 0 || x % (w-1) == 0 || y == 0 || x == 0 then
                        "#"
                    else
                        "."
                    end
         end
-        str += "\n" if x != h-1
+        str += "\n" if y != h-1 
     end
 
     return str
