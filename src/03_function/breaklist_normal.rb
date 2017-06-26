8049b4800cc4449e19a69382d8eddfb88e9b07cc
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-27 00:08:41 +0900

    Fix 03_function-breaklist_normal

--- src/03_function/breaklist_normal.rb
+++ src/03_function/breaklist_normal.rb
@@ -1,4 +1,7 @@
 # You can not use loop
+require_relative './drop_while_normal.rb'
+require_relative './take_while_normal.rb'
 
 def breaklist(arr, &block)
+    [take_while(arr) {|x| !yield(x)}, drop_while(arr) {|x| !yield(x)}]
 end
