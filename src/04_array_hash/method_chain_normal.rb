dcfab4ccdd4062c2f6d757930168767ef07c8460
RomTin <micfall.romtin@gmail.com>
2017-06-26 17:22:43 +0900

    add method-chain problem

---
+++ src/04_array_hash/method_chain_normal.rb
@@ -0,0 +1,29 @@
+=begin
+Input: Array(Fixnum)
+1. multiply by 5
+2. subtract by 3
+3. filter odd number
+=end
+def method_chain1(arr)
+  arr
+end
+
+=begin
+Input: Array(String)
+1. convert to uppercase
+2. reverse
+3. remove A, B, C
+=end
+def method_chain2(arr)
+  arr
+end
+
+=begin
+Input: Array
+1. reject all String items
+2. replace all Boolean items by Fixnum(true -> 100, false -> 50)
+3. calculate average of them (use Float)
+=end
+def method_chain3(arr)
+  arr
+end
