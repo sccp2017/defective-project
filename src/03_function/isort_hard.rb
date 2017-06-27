325f4dad39035cbb174cb3e5592d222d3161a9e7
s1250117-tajima <s1250117@u-aizu.ac.jp>
2017-06-28 01:51:41 +0900

    fix 03/isort_hard.rb

--- src/03_function/isort_hard.rb
+++ src/03_function/isort_hard.rb
@@ -3,7 +3,25 @@
 
 # insert(3, [1, 2, 4, 5]) -> [1, 2, 3, 4, 5] 
 def insert(x, arr)
+  if arr == []
+    [x]
+  else
+    head, *tail = arr
+    if head > x
+      [x] + arr
+    else
+      [head] + insert(x, tail)
+    end
+  end
 end
 
+
+
 def isort(arr)
+  if arr.empty?
+    []
+  else
+    head, *tail = arr
+    insert(head, isort(tail))
+  end
 end
