15165e70f065bad3b9a7dcfeda0342603101fa64
sXX1250141-Hida <s1250141@u-aizu.ac.jp>
2017-06-29 19:03:04 +0900

    fix 03_function/complex_sum_demo.rb

--- src/03_function/complex_sum_demo.rb
+++ src/03_function/complex_sum_demo.rb
@@ -5,4 +5,30 @@
 # 15        // sum
 
 def complex_sum(n, arr)
+  #  odd_nums = []
+  
+  #    odd_nums.push(x)
+  #    end
+  #  end
+  
+  #  added_nums =[]
+  #  for x in odd_nums do
+  #    added_nums.push(x + n)
+  #  end
+  # sum = 0
+  # for x in added_nums
+  # sum += x
+  # end
+  sum = 0
+  for x in arr do
+    if x%2 != 0 then
+      sum = sum + x + n
+    end
+  end
+  sum
 end
+
+
+
+odd_nums = filter(arr) (|x| x % 2 == 0)
+added_nums = map(odd_nums) (|x| x + n )
