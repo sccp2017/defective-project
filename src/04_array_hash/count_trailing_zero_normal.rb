e8e683b01167929b76a7d2cc00d6285121b25f42
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-06-30 01:46:18 +0900

    Fix 04_array_hash-count_trailing_zero_normal

--- src/04_array_hash/count_trailing_zero_normal.rb
+++ src/04_array_hash/count_trailing_zero_normal.rb
@@ -1,2 +1,10 @@
 def count_trailing_zero(num)
+    d = num.to_s(2).chars
+    d.inject(0){ |acc, x|
+        if x == '1'
+            acc = 0
+        else
+            acc += 1
+        end
+    }
 end
