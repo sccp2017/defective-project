4d6d68a89b3d5bb1d2166af056e58437dc755fb9
s1250068-ogawa <s1250068@u-aizu.ac.jp>
2017-06-29 10:08:01 +0900

    fix 03_function/square_easy

--- src/03_function/swap_words_demo.rb
+++ src/03_function/swap_words_demo.rb
@@ -4,3 +4,29 @@
 # /[a-z]/ === "A"     => false
 # "a".upcase => "A"
 # "B".downcase => "b"
+def swap_words(words)
+  swap_words = []
+
+  for word in words
+    swapped_words.push(swap_word(word))
+  end
+  swapped_words
+end
+
+def swap_word(word)
+  swapped_word = ''
+  for c in word.chars
+    swapped_word += swap_char(c)
+  end
+  swapped_word
+end
+
+def swap_char(c)
+  if /[a-z]/ === c
+    c.upcase
+  elsif /[A-Z]/ === c
+    c.downcase
+  else
+    ''
+  end
+end
