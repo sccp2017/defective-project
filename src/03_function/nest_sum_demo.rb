cfc0b85c86d880a16b4cbe30940ec652872cf5df
sXs1250010-Sato <s1250010@u-aizu.ac.jp>
2017-06-29 18:28:57 +0900

    solved

--- src/03_function/nest_sum_demo.rb
+++ src/03_function/nest_sum_demo.rb
@@ -1,5 +1,36 @@
 # hint: use .class
 
 def nest_sum(arr)
+=begin  sum = 0
+for n in arr
+  if n.class == Array
+    for m in n
+        sum += m
+    end
+  else n.class == Fixnum
+    sum += n
+  end
+  sum
+end
+=end
+
+  if arr.empty?
+    0
+  else
+    head , *tail =arr
+    (if head.class == Array
+      nest_sum(head)
+    else head)
+      + nest_sum(tail)
+  end
+sum
 end
 
+=begin else
+  for x in arr
+      sum += x
+  end
+end
+  sum
+end
+=end
