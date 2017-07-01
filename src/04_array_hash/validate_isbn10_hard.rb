bbc84b3cb71ddfa92b08578dfd4f7af26f4be2d6
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-07-02 00:31:38 +0900

    Fix 04_array_hash-validate_isbn10_hard

--- src/04_array_hash/validate_isbn10_hard.rb
+++ src/04_array_hash/validate_isbn10_hard.rb
@@ -1,3 +1,11 @@
 def validate_isbn10(isbn)
-  true
+    head = isbn.chars[0, 9]
+    sum = 0
+    head.each_with_index{|item, i| sum += item.to_i * (10 - i)}
+    bottom = if isbn.chars[-1] == 'X'
+                 10
+             else
+                 isbn.chars[-1].to_i
+             end
+    (sum % 11) + bottom == 11
 end
