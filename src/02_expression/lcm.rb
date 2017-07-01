fc526be4a92e2ac52a221f59826175375b88c5ab
sXX1250245-hoshi <s1250245@u-aizu.ac.jp>
2017-07-02 01:43:02 +0900

    fix 02_expression/lcm

--- src/02_expression/lcm.rb
+++ src/02_expression/lcm.rb
@@ -1,3 +1,10 @@
 def lcm(a, b)
-  a < b ? a : b
+
+  for i in 1..((a > b)? a : b)/2 do
+    if a%i==0 && b%i==0
+      gcd=i
+    end
+  end
+  res = a*b/gcd
+    
 end
--- src/03_function/complex_sum_demo.rb
+++ src/03_function/complex_sum_demo.rb
@@ -1,3 +1,6 @@
+require_relative './filter_normal'
+require_relative './map_normal'
+require_relative './inject_normal'
 
 # complex_sum(2, [1, 2, 3, 4, 5]) =>
 # [1, 3, 5] // select odd numbers.
@@ -5,4 +8,25 @@
 # 15        // sum
 
 def complex_sum(n, arr)
+
+  odd_nums = fileter(arr) { |x| x % 2 != 0 }
+  added_nums = map(odd_nums) { |x| x + n }
+  sum = inject(added_nums) { |acc, x| acc + n }
+
+  
+=begin
+  sum = 0
+  if arr.empty?
+    0
+  else    
+    head, *tail = arr
+    if head%2==1
+      head + n + complex_sum(n, tail)
+    else
+      complex_sum(n, tail)
+    end
+
+  end
+=end
+
 end
