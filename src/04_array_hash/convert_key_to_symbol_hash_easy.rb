ff5cb1a16f0f5a408fc6a08384fd6946da94d4dd
manga_osyo <manga.osyo@gmail.com>
2017-07-06 06:19:52 +0900

    Add e.g.

--- src/04_array_hash/convert_key_to_symbol_hash_easy.rb
+++ src/04_array_hash/convert_key_to_symbol_hash_easy.rb
@@ -1,6 +1,8 @@
 # Convert key to symbol
 # input  : Hash
 # output : Hash
+# e.g.   :
+#   convert_key_to_symbol({ "name" => "homu", "age" => 14 }) => {:name=>"homu", :age=>14}
 def convert_key_to_symbol hash
   Hash[ hash.map{ |k, v| [k.to_sym, v] } ]
 end
