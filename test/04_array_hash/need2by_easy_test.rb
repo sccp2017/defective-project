e950fbc52484798eee716af1b7b91d3ccb5dde7b
F_Haga <s1240056@gmail.com>
2017-07-06 15:35:38 +0900

    fix variable

--- test/04_array_hash/need2by_easy_test.rb
+++ test/04_array_hash/need2by_easy_test.rb
@@ -6,29 +6,29 @@ class TypeTest < Test::Unit::TestCase
     already = [1, 3, 4, 6]
     sell = [1, 2, 4, 7, 8]
 
-    need = need2by(already, sell)
+    actual = need2by(already, sell)
 
-    expect = [2, 7, 8]
-    assert_equal need, expect
+    expected = [2, 7, 8]
+    assert_equal actual, expected
   end
 
   def test_need2by_cond2
     already = [1, 2, 6, 7, 9]
     sell = [3, 4, 6, 9, 10]
 
-    need = need2by(already, sell)
+    actual = need2by(already, sell)
 
-    expect = [3, 4, 10]
-    assert_equal need, expect
+    expected = [3, 4, 10]
+    assert_equal actual, expected
   end
 
   def test_need2by_cond3
     already = [1, 4, 5, 6, 8, 10, 11, 13]
     sell = [1, 3, 6, 10, 15]
 
-    need = need2by(already, sell)
+    actual = need2by(already, sell)
 
-    expect = [3, 6, 8, 15]
-    assert_equal need, expect
+    expected = [3, 6, 8, 15]
+    assert_equal actual, expected
   end
 end
