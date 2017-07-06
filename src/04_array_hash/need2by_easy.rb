28fc5c47d4d505fbcde2cb424756e200ac381236
F_Haga <s1240056@gmail.com>
2017-07-06 14:40:27 +0900

    add need2by prob

---
+++ src/04_array_hash/need2by_easy.rb
@@ -0,0 +1,25 @@
+def need2by(already,sell)
+
+=begin
+
+  need2by(already,sell)
+
+  Make a list of books you should buy
+
+  example(1)
+
+  already = [1,3,4,6]
+  sell = [1,2,4,7,8]
+  
+  need = [2,7,8]
+  
+  example(2)
+
+  already = [1,2,6,7,9]
+  sell = [3,4,6,9,10]
+  
+  need = [3,4,10]
+=end
+
+    need
+end
---
+++ test/04_array_hash/need2by_easy_test.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require_relative '../../src/04_array_hash/need2by_easy'
+
+class TypeTest < Test::Unit::TestCase
+    def test_need2by_cond1
+        already = [1,3,4,6]
+        sell = [1,2,4,7,8]
+
+        need = need2by(already,sell)
+
+        expect = [2,7,8]
+        assert_equal need, expect
+
+    end
+
+    def test_need2by_cond2
+        already = [1,2,6,7,9]
+        sell = [3,4,6,9,10]
+
+        need = need2by(already,sell)
+
+        expect = [3,4,10]
+        assert_equal need, expect
+
+    end
+
+    def test_need2by_cond3
+        already = [1,4,5,6,8,10,11,13]
+        sell = [1,3,6,10,15]
+
+        need = need2by(already,sell)
+
+        expect = [3,6,8,15]
+        assert_equal need, expect
+
+    end
+end
