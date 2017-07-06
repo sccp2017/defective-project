17d0f17cb5dd5249fd07752f173cf2460d8b4a21
F_Haga <s1240056@gmail.com>
2017-07-06 15:07:54 +0900

    fix coding style (src/)

--- src/04_array_hash/need2by_easy.rb
+++ src/04_array_hash/need2by_easy.rb
@@ -1,25 +1,6 @@
-def need2by(already,sell)
-
-=begin
-
-  need2by(already,sell)
-
-  Make a list of books you should buy
-
-  example(1)
-
-  already = [1,3,4,6]
-  sell = [1,2,4,7,8]
-  
-  need = [2,7,8]
-  
-  example(2)
-
-  already = [1,2,6,7,9]
-  sell = [3,4,6,9,10]
-  
-  need = [3,4,10]
-=end
-
-    need
+def need2by(already, sell)
+  # need2by(already, sell)
+  # Make a list of books you should buy
+  need = already + sell
+  need
 end
