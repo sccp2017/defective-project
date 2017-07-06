8cf3632a7b6b2c89a4a5f0cc56ec27e958af78cb
F_Haga <s1240056@gmail.com>
2017-07-06 15:31:52 +0900

    fix coding style (test/)

--- test/04_array_hash/need2by_easy_test.rb
+++ test/04_array_hash/need2by_easy_test.rb
@@ -2,36 +2,33 @@ require 'test/unit'
 require_relative '../../src/04_array_hash/need2by_easy'
 
 class TypeTest < Test::Unit::TestCase
-    def test_need2by_cond1
-        already = [1,3,4,6]
-        sell = [1,2,4,7,8]
+  def test_need3by_cond1
+    already = [1, 3, 4, 6]
+    sell = [1, 2, 4, 7, 8]
 
-        need = need2by(already,sell)
+    need = need2by(already, sell)
 
-        expect = [2,7,8]
-        assert_equal need, expect
+    expect = [2, 7, 8]
+    assert_equal need, expect
+  end
 
-    end
+  def test_need2by_cond2
+    already = [1, 2, 6, 7, 9]
+    sell = [3, 4, 6, 9, 10]
 
-    def test_need2by_cond2
-        already = [1,2,6,7,9]
-        sell = [3,4,6,9,10]
+    need = need2by(already, sell)
 
-        need = need2by(already,sell)
+    expect = [3, 4, 10]
+    assert_equal need, expect
+  end
 
-        expect = [3,4,10]
-        assert_equal need, expect
+  def test_need2by_cond3
+    already = [1, 4, 5, 6, 8, 10, 11, 13]
+    sell = [1, 3, 6, 10, 15]
 
-    end
+    need = need2by(already, sell)
 
-    def test_need2by_cond3
-        already = [1,4,5,6,8,10,11,13]
-        sell = [1,3,6,10,15]
-
-        need = need2by(already,sell)
-
-        expect = [3,6,8,15]
-        assert_equal need, expect
-
-    end
+    expect = [3, 6, 8, 15]
+    assert_equal need, expect
+  end
 end
