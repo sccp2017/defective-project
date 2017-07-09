3a58cbfbb2fda3cb7ee76d58aff35a2b82fe94c7
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-07-09 15:17:47 +0900

    04_array_hash-check_miraban_normal

--- src/04_array_hash/check_miraban_normal.rb
+++ src/04_array_hash/check_miraban_normal.rb
@@ -10,4 +10,8 @@
 #   check_miraban(123)   => false
 #   check_miraban(1232)  => false
 def check_miraban num
+    arr = num.abs.to_s.chars.map{|x| x.to_i}
+    top = arr[0..(arr.length / 2)]
+    bottom = arr[(-arr.length / 2)..-1].reverse
+    top == bottom
 end
