61dcba7d06971f8291477081df9c225625bf538e
s1250010-Sato <s1250010@u-aizu.ac.jp>
2017-07-10 09:49:38 +0900

    fix 04/manual_check

--- src/04_array_hash/manual_check_easy.rb
+++ src/04_array_hash/manual_check_easy.rb
@@ -1,5 +1,9 @@
 # what will hash return when undefined keys accessed, you know ?
 def manual_check(key)
   dict = {Apple:'Red', Banana:'Yellow', 松崎しげる:'Black'}
-
+  if dict[:"#{key}"] == nil
+    return "not know"
+  else
+    return dict[:"#{key}"]
+  end
 end
