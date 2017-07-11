63729cd9dec2b97953c6e84f63666ed2f23a007e
s1250087-oba <s1250087@u-aizu.ac.jp>
2017-07-11 17:56:39 +0900

    Fix

--- src/04_array_hash/msort_hard.rb
+++ src/04_array_hash/msort_hard.rb
@@ -12,7 +12,25 @@
 # [1, 2, 5, 4, 6, 3]
 
 def merge(arr1, arr2)
+    if arr1.empty? && arr2.empty?
+        []
+    else
+        head1, *tail1 = arr1
+        head2, *tail2 = arr2
+        if head1 < head2
+            [head1] + merge(tail1, arr2)
+        else
+            [head2] + merge(arr1, tail2);
+        end
+    end
 end
 
 def msort(arr)
+    top = arr[0, arr.size/2]
+    bottom = arr[arr.size/2, arr.size]
+    if arr.size <= 3
+        merge(top, bottom)
+    else
+        msort(top) + msort(bottom)
+    end
 end
