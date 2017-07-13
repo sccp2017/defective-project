373260f209e299da895a894734aec8a8f203277b
sXX1240073-Tsuboi <s1240073@u-aizu.ac.jp>
2017-07-13 19:08:29 +0900

    fix 02/lcm

--- src/02_expression/lcm.rb
+++ src/02_expression/lcm.rb
@@ -1,3 +1,17 @@
 def lcm(a, b)
-  a < b ? a : b
+  judge = a < b ? a : b
+  if a < b
+    a * b/gcd(a,b)
+  else
+    b * a/gcd(a,b)
+  end
 end
+
+def gcd(a,b)
+  if b==0
+    a
+  else 
+    gcd(b,a%b)
+  end
+end
+  
