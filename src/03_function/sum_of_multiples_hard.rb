a7481ee77feac5e79bc052493715f073fc2d53f8
s1230171-kiji <s1230171@u-aizu.ac.jp>
2017-07-27 19:29:58 +0900

    fix 03_sum_of_multiples_hard

--- src/03_function/sum_of_multiples_hard.rb
+++ src/03_function/sum_of_multiples_hard.rb
@@ -1,7 +1,23 @@
 def sum_of_multiples(num_list, up_to)
-  1
+  arr = Array.new
+  num_list.each {|n| arr.concat(multiples(n,up_to))}
+  arr2 = arr.uniq
+  sum = 0
+  arr2.each {|n| sum = sum + n }
+  sum
+  
+  
 end
 
 def multiples(num, up_to)
-  1
+  arr = Array.new
+  i = num
+  while i < up_to do
+    arr.push(i)
+    i = i + num
+  end
+  arr
 end
+
+
+
